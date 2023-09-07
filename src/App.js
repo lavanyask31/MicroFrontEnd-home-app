c3a310fa7a4bb81ae764ba4ffd07aa14c612af86
Lavanya S K <lavanya.sk@updaptcsr.com>
2023-09-07 15:40:28 +0530

    setup 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import "./App.css";
+
+const Header = lazy(() => import("HeaderApp/Header"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Header />
+      </Suspense>
+
+      <div>Demo Heome Page</div>
     </div>
   );
 }
